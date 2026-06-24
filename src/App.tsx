78749bd7371141b88549596f14c745bf9641559b
Parin Patel <parinpatel4606@gmail.com>
2026-06-24 14:27:52 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Grid, GridItem, Show, Stack } from "@chakra-ui/react";
 const App = () => {
   return (
     <div>
-      <Button>Button</Button>
+      <Grid
+        gridTemplateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="blue">
+          Nav
+        </GridItem>
+        <Stack hideBelow="lg">
+        <GridItem area="aside" bg="green">
+          Aside
+        </GridItem>
+        </Stack>
+        <GridItem area="main" bg="red">
+          Main
+        </GridItem>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider value={defaultSystem}>
       <App/>
-    </Provider>
+    </ChakraProvider>
   </StrictMode>,
 )
